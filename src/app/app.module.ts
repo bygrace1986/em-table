cb4dc18bb154a4df564b11d9e1eb42daac0ed56c
Erik Murphy <erik.murphy@boschbgt.com>
2018-06-26 13:15:25 -0400

    removing @em-test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { EmTableModule } from '@em-table/em-table.module';
+import { EmTableModule } from 'em-table/em-table.module';
 import { AppComponent } from './app.component';
 import { TableDemoComponent } from './table-demo/table-demo.component';
-import { DataTableConfig, DATA_TABLE_CONFIG } from '@em-table/models';
+import { DataTableConfig, DATA_TABLE_CONFIG } from 'em-table/models';
 import { ResizeService } from './resize.service';
 
 @NgModule({
